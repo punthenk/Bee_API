45fd0f7542473838cea8a13474f64811cafa8c89
MichielN19 <Michielmitsjol@gmail.com>
2025-11-30 17:46:39 +0100

    feat(routes): added get route for specific hive

--- src/routes.rs
+++ src/routes.rs
@@ -6,5 +6,6 @@ use crate::controllers::hive_controller;
 pub fn create_routes(pool: MySqlPool) -> Router {
     Router::new()
         .route("/hives", get(hive_controller::get_all))
+        .route("/hive/{id}", get(hive_controller::find))
         .with_state(pool)
 }
