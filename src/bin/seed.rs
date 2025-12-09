786a8b78d659bed4b57d4b243aeae7c3a6a0fec5
MichielN19 <Michielmitsjol@gmail.com>
2025-12-09 08:40:03 +0100

    feat(seeder): added seed for queen

--- src/bin/seed.rs
+++ src/bin/seed.rs
@@ -5,6 +5,7 @@
 use sqlx::mysql::MySqlPoolOptions;
 
 use beekeeper_API::models::hive::Hive;
+use beekeeper_API::models::queen::Queen;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -24,27 +25,48 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     
     // Clear all existing data
     Hive::delete_all(&pool).await?;
+    Queen::delete_all(&pool).await?;
 
     println!("Inserting seed data...");
 
-    // Insert test hives
-    let hive_count = 15;
 
-    for i in 1..=hive_count {
-        let hive = Hive {
+    // Insert test queens
+    let queen_count = 4;
+
+    for i in 1..=queen_count {
+        let queen = Queen {
             id: 1, // NOT USED IN QUERY
-            user_id: 1,
-            queen_id: 1,
-            name: format!("Hive {}", i),
-            temperature: None, // NOT USED IN QUERY
-            humidity: None, // NOT USED IN QUERY
+            race: "Race".to_string(),
+            origin: Some("Nature".to_string()),
+            birth_year: 2025,
+            fertilization_site: None,
+            clipped: true,
             created_at: None, // NOT USED IN QUERY
             updated_at: None, // NOT USED IN QUERY
         };
 
-        Hive::add(&pool, hive).await.expect("Failed to add hive");
-        println!("Added: Hive {}", i);
+        Queen::add(&pool, queen).await.expect("Failed to add queen");
+        println!("Added: Queen {}", i);
     }
+
+    // Insert test hives
+    // let hive_count = 15;
+    //
+    // for i in 1..=hive_count {
+    //     let hive = Hive {
+    //         id: 1, // NOT USED IN QUERY
+    //         user_id: 1,
+    //         queen_id: 1,
+    //         name: format!("Hive {}", i),
+    //         temperature: None, // NOT USED IN QUERY
+    //         humidity: None, // NOT USED IN QUERY
+    //         created_at: None, // NOT USED IN QUERY
+    //         updated_at: None, // NOT USED IN QUERY
+    //     };
+    //
+    //     Hive::add(&pool, hive).await.expect("Failed to add hive");
+    //     println!("Added: Hive {}", i);
+    // }
 
     println!("Seeding complete");
     Ok(())
