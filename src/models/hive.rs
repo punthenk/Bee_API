4b2925fe026b95e4f479fc4404614f527d80c286
MichielN19 <Michielmitsjol@gmail.com>
2025-12-10 16:20:25 +0100

    feat(response): added response to add hive

--- src/models/hive.rs
+++ src/models/hive.rs
@@ -3,7 +3,7 @@ use sqlx::{FromRow, MySqlPool, Result, Error}; // FromRow is a SQLx trait that a
 use chrono::{DateTime, Utc};
 
 
-#[derive(Debug, Serialize, Deserialize, FromRow)]
+#[derive(Debug, Serialize, Deserialize, FromRow, Clone)]
 pub struct Hive {
     #[serde(skip_deserializing)] // Don't expect the id from a form
     pub id: i32,
