9fdfef343ec41caacb4123df12696fd401631d7f
MichielN19 <Michielmitsjol@gmail.com>
2025-12-12 13:16:48 +0100

    update(response): added response to inspection controller

--- src/controllers/inspection_controller.rs
+++ src/controllers/inspection_controller.rs
@@ -8,24 +8,19 @@ use axum::{
 use sqlx::MySqlPool;
 
 use crate::models::inspection::{Inspection};
+use crate::response::{ApiResponse, ApiError};
 
-pub async fn get_all(State(pool): State<MySqlPool>) -> Result<Json<Vec<Inspection>>, StatusCode> {
+pub async fn get_all(State(pool): State<MySqlPool>) -> Response {
     match Inspection::get_all(&pool).await {
-        Ok(inspections) => Ok(Json(inspections)),
-        Err(e) => {
-            eprintln!("Database error: {:?}", e);
-            Err(StatusCode::INTERNAL_SERVER_ERROR)
-        }
+        Ok(inspections) => ApiResponse::new(inspections, StatusCode::OK).into_response(),
+        Err(e) => ApiError::internal_error(format!("Database error {:?}", e)),
     }
 }
 
-pub async fn find(State(pool): State<MySqlPool>, Path(id): Path<i32>) -> Result<Json<Inspection>, StatusCode> {
+pub async fn find(State(pool): State<MySqlPool>, Path(id): Path<i32>) -> Response {
     match Inspection::find(&pool, id).await {
-        Ok(Some(inspection)) => Ok(Json(inspection)),
-        Ok(None) => Err(StatusCode::NOT_FOUND),
-        Err(e) => {
-            eprintln!("Database error: {:?}", e);
-            Err(StatusCode::INTERNAL_SERVER_ERROR)
-        }
+        Ok(Some(inspection)) => ApiResponse::new(inspection, StatusCode::OK).into_response(),
+        Ok(None) => ApiError::not_found("Inspection not found"),
+        Err(e) => ApiError::internal_error(format!("Database error: {:?}", e)),
     }
 }
