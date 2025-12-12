3edd450dbf83af2a3799249498a6a5fad01cb0f7
MichielN19 <Michielmitsjol@gmail.com>
2025-12-12 13:21:33 +0100

    fix(routes): fixed queen route
    I deleted the / at the end because none of the routes have that

--- src/routes.rs
+++ src/routes.rs
@@ -10,7 +10,7 @@ pub fn create_routes(pool: MySqlPool) -> Router {
         // GET
         .route("/hives", get(hive_controller::get_all))
         .route("/hive/{id}", get(hive_controller::find))
-        .route("/queens/", get(queen_controller::get_all))
+        .route("/queens", get(queen_controller::get_all))
         .route("/inspections", get(inspection_controller::get_all))
         .route("/inspection/{id}", get(inspection_controller::find))
 
