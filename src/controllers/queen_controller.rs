cf8a4593c4e8229c9fa132748697cab7b4e62b95
MichielN19 <Michielmitsjol@gmail.com>
2025-12-12 13:38:27 +0100

    fix(response): fixed some mistakes and inconsistencies

--- src/controllers/queen_controller.rs
+++ src/controllers/queen_controller.rs
@@ -11,7 +11,7 @@ use crate::response::{ApiResponse, ApiError};
 
 pub async fn get_all(State(pool): State<MySqlPool>) -> Response {
     match Queen::get_all(&pool).await {
-        Ok(hives) => ApiResponse::new(hives, StatusCode::OK).into_response(),
-        Err(e) => ApiError::not_found(format!("Database error {:?}", e)),
+        Ok(queens) => ApiResponse::new(queens, StatusCode::OK).into_response(),
+        Err(e) => ApiError::internal_error(format!("Database error: {:?}", e)),
     }
 }
